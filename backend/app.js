c521f645499b71683be3a402201b88118ca1d805
alssdev <alssdev3@gmail.com>
2020-09-29 00:05:39 +0200

    Preparación final

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,6 @@ const exphbs = require('express-handlebars');
 
 const path = require('path');
 
-const morgan = require('morgan');
-
 // intialiaztions
 const app = express();
 
@@ -12,19 +10,17 @@ const app = express();
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views')); // here are views
 app.engine(
-	'.hbs',
-	exphbs({
-		defaultLayout: 'main',
-		layoutsDir: path.join(app.get('views'), 'layouts'),
-		partialsDir: path.join(app.get('views'), 'partials'),
-		extname: '.hbs', // files' extentions
-	})
+  '.hbs',
+  exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs', // files' extentions
+  })
 );
 app.set('view engine', '.hbs');
 
 // middlewares
-
-app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false })); // accept only simple data
 app.use(express.json());
 
